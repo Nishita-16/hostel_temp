576cb8707382e5d40387847dfba2246c6634a2e7
Nishita-16 <nishuverma105514@gmail.com>
2026-03-14 22:55:32 +0530

    change server.js add url

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use("/api", uploadRoutes);
 // ==========================
 app.use(
   cors({
-    origin: "http://localhost:8080",
+    origin:["http://localhost:8080","https://hostel-temp-frontend.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
